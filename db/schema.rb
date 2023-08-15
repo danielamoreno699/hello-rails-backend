fc773d8952f8833229a9ee5cd3a8e873976bef1e
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 18:32:55 -0500

    create database

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_180423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # create_table "greetings", force: :cascade do |t|
-  #   t.string "content"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
+  create_table "greetings", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
