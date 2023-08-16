1e93a17273b9ac756f306450a1308077bf972b6f
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 20:18:19 -0500

    update cors with 3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3002' # Replace with your React app's URL
+      origins 'http://localhost:3001' # Replace with your React app's URL
      
       resource '/api/v1/*',
         headers: :any,
